9c62cbe7f7265dd3207fac1c6ae89cb307d16d8f
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-05-31 19:23:10 +0000

    Addition of TicketPriceCalculator.cs

---
+++ TicketPriceCalculator.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int regularPrice = 10;
+            const int discountedPrice = 7;
+
+            Console.WriteLine("Enter your age: ");
+
+            if (int.TryParse(Console.ReadLine(), out int age) && age >= 0)
+            {
+                int ticketPrice = (age <= 12 || age >= 65) ? discountedPrice : regularPrice;
+                Console.WriteLine("The ticket price is: GHC" + ticketPrice);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid age.");
+            }
+        }
+    }
+}
